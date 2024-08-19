35031069533edac24e34a3f5eff60ab8cc74bfcd
syedarsalanshah <syedarsalanshah02@gmail.com>
2024-08-19 21:57:47 +0500

    First commit

---
+++ src/components/MetaVerseFrame.tsx
@@ -0,0 +1,34 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { useEffect } from "react";
+
+const MetaVerseFrame = () => {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+  });
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  return (
+    <Unity
+      unityProvider={unityProvider}
+      style={{
+        width: "100%",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    />
+  );
+};
+
+export default MetaVerseFrame;
